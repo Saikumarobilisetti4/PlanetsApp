e32a4ef4373850a29229daaf4a34ba83ba5b74f5
Sai kumar Obilisetti <saikumars134@gmail.com>
2023-07-29 16:52:20 +0000

    Planets-App

---
+++ src/components/PlanetsSlider/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-image: url('https://assets.ccbp.in/frontend/react-js/planets-app/planets-bg-img.png');
+  min-height: 100vh;
+  padding: 40px;
+`
+export const MainHeading = styled.h1`
+  color: #05acff;
+  font-size: 25px;
+  text-align: center;
+  font-family: 'Roboto';
+`
